ed577802af0a8f600afc956342f61a3ae8046166
Rakib Hasan <kmrakib168@gmail.com>
2020-11-28 01:42:25 +0600

    Add interactive feature to the banner

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/* =======================================
+    CHANGE SHOCASE PICTURE.
+ ========================================= */
+
+let currentPic = 1;
+
+function displayNext() {
+    // replace the current picture
+    let picId = `img/${currentPic}.jpg`;
+    // alert(picId)
+    document.getElementById('showcase-img').src=picId;
+    // if(currentPic >= 234) currentPic = 0;
+    currentPic += 1;
+}
